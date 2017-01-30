54a4e4ed42092b9eeeb769cc4ec5e5da8bbf392e
pixlKing <campagnamatias@gmail.com>
2017-01-29 22:28:19 -0300

    impreso los datos con angular

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,10 +1,12 @@
 var app = angular.module("app", []);
 
 app.controller("appController", function ($scope, $http) {
-   //vamos a hacer uso de $http para obtener los datos
-   $http.get('/almundo/json/data.json').success(function (data) {
-   //enviamos los datos a la vista con el objeto $scope
-   $scope.datos = data;
- });
+	//vamos a hacer uso de $http para obtener los datos
+	$http.get('/almundo/json/data.json').success(function (data) {
+		//enviamos los datos a la vista con el objeto $scope
+		$scope.datos = data;
+
+		console.log(data);
+	});
 })
 
