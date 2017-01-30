a4c6b41efc5f16fb81613633eea860c87717729f
pixlKing <campagnamatias@gmail.com>
2017-01-29 21:21:53 -0300

    testeando la consumicion del json

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,10 @@
 var app = angular.module("app", []);
 
+app.controller("appController", function ($scope, $http) {
+   //vamos a hacer uso de $http para obtener los datos
+   $http.get('/almundo/json/data.json').success(function (data) {
+   //enviamos los datos a la vista con el objeto $scope
+   $scope.datos = data;
+ });
+})
+
