ee45e7cf27729903dcad90b6e7d099d673d1c26e
PixlKing <campagnamatias@gmail.com>
2018-08-14 19:14:57 -0300

    fixeados rutas relativas que estaban rotas

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ var app = angular.module("app", []);
 
 app.controller("appController", function ($scope, $http) {
 
-	$http.get('/almundo/api/data.json').success(function (data) {
+	$http.get('/search-result/api/data.json').success(function (data) {
 
 		$scope.hotels = data.hotels;
 
